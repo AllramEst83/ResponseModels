b9f755255b7f4d48b9f05ecbde9596ff9f17c799
kay Wiberg <kaywib@hotmail.com>
2019-03-02 23:21:09 +0100

    La till en parameterlös konstruktor till "GetAllRolesResponse"

--- ResponseModels/ViewModels/GetAllRolesResponse.cs
+++ ResponseModels/ViewModels/GetAllRolesResponse.cs
@@ -7,7 +7,20 @@ namespace ResponseModels.ViewModels
 {
     public class GetAllRolesResponse : ResponseModelBase
     {
-        public GetAllRolesResponse(int _statusCode) : base(_statusCode) { }
+        public GetAllRolesResponse(){ }
+
+        public GetAllRolesResponse(
+            int _statusCode,
+            string _error,
+            string _description,
+            string _code
+            ) : base(
+                _statusCode,
+                _error,
+                _description,
+                _code
+                )
+        { }
 
         public List<GetAllRoles> ListOfAllRoles { get; set; }
     }
