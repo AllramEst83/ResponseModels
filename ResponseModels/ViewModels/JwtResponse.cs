d3927caa9c8ca1b0704be1453684f564fd038e05
kay Wiberg <kaywib@hotmail.com>
2019-03-05 19:36:47 +0100

    Refactor done

---
+++ ResponseModels/ViewModels/JwtResponse.cs
@@ -0,0 +1,29 @@
+﻿using ResponseModels.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ResponseModels.ViewModels
+{
+   public class JwtResponse : ResponseModelBase
+    {
+        public JwtResponse() { }
+
+        public JwtResponse(
+            int _statusCode,
+            string _error,
+            string _description,
+            string _code
+            ) : base(
+                _statusCode,
+                _error,
+                _description,
+                _code
+                )
+        { }
+
+        public string Id { get; set; }
+        public string Auth_Token { get; set; }
+        public int Expires_In { get; set; }
+    }
+}
---
+++ ResponseModels/ViewModels/SignUpResponse.cs
@@ -0,0 +1,25 @@
+﻿using ResponseModels.Models;
+
+namespace ResponseModels.ViewModels
+{
+    public class SignUpResponse : ResponseModelBase
+    {
+        public SignUpResponse() { }
+
+        public SignUpResponse(
+            int _statusCode,
+            string _error,
+            string _description,
+            string _code
+            ) : base(
+                _statusCode,
+                _error,
+                _description,
+                _code
+                )
+        { }
+
+        public string Id { get; set; }
+        public string Email { get; set; }
+    }
+}
